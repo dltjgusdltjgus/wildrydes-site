e86f47a569057a7078c1692795b65ca0399e5391
EC2 Default User <ec2-user@ip-172-31-43-103.ap-northeast-2.compute.internal>
2025-02-27 07:05:22 +0000

    v1

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-2_weHUWuHj0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4cchekdclt6oddb2s1i83j6943', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_qHF8cgIfo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2bqg2getbjqitg50a054pbonfi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://e7khzzwnr8.execute-api.ap-northeast-2.amazonaws.com/pruod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
